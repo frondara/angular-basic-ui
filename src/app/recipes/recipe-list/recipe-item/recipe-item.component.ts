42aee514de16bfedd326f06305d6c515b3d5aa8b
frondara <arvinfrondarina@talapparel.com>
2024-03-15 19:53:38 +0800

    downgrade to bootstrap 3 and redesign header

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-recipe-item',
   templateUrl: './recipe-item.component.html',
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrl: './recipe-list.component.css'
 })
-export class RecipeListComponent {
+export class RecipeListComponent implements OnInit{
+  
+  recipes: Recipe[]= [
+    new Recipe('A test recipe','This is a test description',
+    'https://cdn.pixabay.com/photo/2015/10/26/07/21/vegetables-1006694_1280.jpg'),
+    new Recipe('A test recipe','This is a test description',
+    'https://cdn.pixabay.com/photo/2015/10/26/07/21/vegetables-1006694_1280.jpg'),
+    new Recipe('A test recipe','This is a test description',
+    'https://cdn.pixabay.com/photo/2015/10/26/07/21/vegetables-1006694_1280.jpg')
+  ];
 
+  ngOnInit(): void {
+    
+  }
 }
